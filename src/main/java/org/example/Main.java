5abb81067c226bc6c7ecb3988fdf82b4db728000
danielpaluch <danielpaluch37@gmail.com>
2023-01-20 16:37:08 +0100

    additions

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,11 @@ package org.example;
 
 import static com.mongodb.MongoClientSettings.getDefaultCodecRegistry;
 import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Projections.include;
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
-import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.*;
 import org.bson.BsonDocument;
 import org.bson.Document;
 import org.bson.codecs.configuration.CodecProvider;
@@ -21,6 +22,7 @@ import javax.print.Doc;
 import javax.swing.event.DocumentEvent;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 import java.security.SecureRandom;
@@ -110,22 +112,90 @@ public class Main {
         System.out.println("Hasło użytkownika: ");
         String paswd = scanner.nextLine();
 
-
         Document user = users.find(eq("name", name))
                 .first();
 
+        if(user == null){
+            System.out.println("Nieprawidłowe dane.");
+            mainMenu(users);
+        }
 
-
-        System.out.println(user.toBsonDocument());
+        if(Objects.equals(user.get("password").toString(), paswd.toString())){
+            System.out.println("Poprawnie zalogowano.");
+            Object userId = user.get("_id");
+            loggedMenu(users, userId);
+        }
+        else{
+            System.out.println("Nieprawidłowe dane.");
+            mainMenu(users);
+        }
 
 
 
     }
+    public static void loggedMenu(MongoCollection<Document> users, Object userId){
+        System.out.println("======================================");
+        System.out.println("1. Dodaj hasło.");
+        System.out.println("2. Wyświetl hasła.");
+        System.out.println("3. Wyloguj.");
+        System.out.println();
+
+
+
+        System.out.println("Wybierz opcje, która chcesz wykonać: ");
+        int option = scanner.nextInt();
+
+        if(option == 1){
+            System.out.println("Wybrano dodawanie hasla.");
+
+            System.out.println();
+            System.out.println("Nazwa strony: ");
+            scanner.nextLine();
+            String name = scanner.nextLine();
+            System.out.println("Login: ");
+            String login = scanner.nextLine();
+            System.out.println("Hasło: ");
+            String paswd = scanner.nextLine();
+
+            Bson filter = Filters.eq("_id", userId);
+            Bson update = Updates.push("sites", new Document()
+                    .append("name", name)
+                    .append("login", login)
+                    .append("password", paswd));
+            FindOneAndUpdateOptions options = new FindOneAndUpdateOptions()
+                    .returnDocument(ReturnDocument.AFTER);
+            Document result = users.findOneAndUpdate(filter, update, options);
+            if(result != null){
+                System.out.println("Pomyślnie dodano do bazy.");
+                loggedMenu(users, userId);
+            }
+            else {
+                System.out.println("Nie dodano do bazy.");
+                loggedMenu(users, userId);
+            }
+        }
+        else if(option==2){
+            Bson projection = include("sites");
+            Document result = users.find(eq("_id", userId))
+                    .projection(projection)
+                    .first();
+            List<Document> list = result.getList("sites", Document.class);
+            for (Document obj : list) {System.out.println(
+                    "Strona: " + obj.get("name").toString() + " " +
+                    "Login: " + obj.get("login").toString() + " " +
+                    "Hasło: " + obj.get("password").toString());}
+            loggedMenu(users, userId);
+        }
+        else if(option==3){
+            System.out.println("Wybrano wylogowanie.");
+            mainMenu(users);
+        }
+        else{
+            System.out.println("Nie ma takiej opcji.");
+            loggedMenu(users, userId);
+        }
+    }
     public static void main(String[] args) {
-
-
-
-
         MongoClient client = MongoClients.create("mongodb+srv://admin:admin@project.ksjanfd.mongodb.net/?retryWrites=true&w=majority");
 
         MongoDatabase db = client.getDatabase("java_project");
